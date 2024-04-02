f31030add3a5b4cba88578122619223750cb2be6
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-04-02 18:02:15 +0200

    Add build-images.sh script for creating container images

---
+++ build-images.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+# Terminate on error
+set -e
+
+# Prepare variables for later use
+images=()
+# The image will be pushed to GitHub container registry
+repobase="${REPOBASE:-ghcr.io/nethserver}"
+# Configure the image name
+reponame="lam"
+
+# Create a new empty container image
+container=$(buildah from scratch)
+
+# Reuse existing nodebuilder-lam container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-lam; then
+    echo "Pulling NodeJS runtime..."
+    buildah from --name nodebuilder-lam -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+fi
+
+echo "Build static UI files with node..."
+buildah run \
+    --workingdir=/usr/src/ui \
+    --env="NODE_OPTIONS=--openssl-legacy-provider" \
+    nodebuilder-lam \
+    sh -c "yarn install && yarn build"
+
+# Add imageroot directory to the container image
+buildah add "${container}" imageroot /imageroot
+buildah add "${container}" ui/dist /ui
+# Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
+buildah config --entrypoint=/ \
+    --label="org.nethserver.tcp-ports-demand=1" \
+    --label="org.nethserver.rootfull=0" \
+    --label="org.nethserver.authorizations=traefik@any:routeadm cluster:accountconsumer" \
+    --label="org.nethserver.images=ghcr.io/ldapaccountmanager/lam:8.7" \
+    "${container}"
+# Commit everything
+buildah commit "${container}" "${repobase}/${reponame}"
+
+images+=("${repobase}/${reponame}")
+
+# Setup CI when pushing to Github. 
+# Warning! docker::// protocol expects lowercase letters (,,)
+if [[ -n "${CI}" ]]; then
+    # Set output value for Github Actions
+    printf "::set-output name=images::%s\n" "${images[*],,}"
+else
+    # Just print info for manual push
+    printf "Publish the images with:\n\n"
+    for image in "${images[@],,}"; do printf "  buildah push %s docker://%s:%s\n" "${image}" "${image}" "${IMAGETAG:-latest}" ; done
+    printf "\n"
+fi
